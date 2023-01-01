94996101e33596a5b3d6ff4d5819c1f0238500db
Giacomo Salici <giambo2000@gmail.com>
2023-01-01 20:47:57 +0100

    basic user auth

--- server/app.py
+++ server/app.py
@@ -3,42 +3,46 @@ from configparser import ConfigParser
 import requests
 import json
 
-app = Flask(__name__)
-dataset=[]
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
-def getBusRoutes(stop_id):
-    with requests.get(f"https://opendata.comune.bologna.it/api/v2/catalog/datasets/tper-vigente-mattina/records?limit=100&offset=0&refine=stop_id%3A{stop_id}&timezone=Europe%2FBerlin") as res:
-        if (res.status_code == 200):
-            routes = [elem["record"]["fields"] for elem in res.json()["records"] ]
-            print(routes)
-            return routes
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
 
 
+def create_app():   
+    app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template('index.html', stations=dataset, name = "Bus Stops Map")
-
-@app.route("/<station>")
-def page(station):
-
-    nametext = "\""+ dataset[station]["name"] + "\"" if dataset[station]["name"] != "" else ""
-    return render_template('station.html', routes = getBusRoutes(station), stations={station: dataset[station]}, name="Bus Stop "+ nametext + " #"+station)
-
-
-
-if __name__ == '__main__':
-   
+  
     
-    
-    with open("./busStopDataset.json", "r") as file:
-        dataset=json.load(file)
 
     with open("./config.ini", "r") as file:
         config = ConfigParser()
         config.read_file(file)
-        flask_config = dict(config.items("Flask"))
+        flask_config = dict(config.items("flask"))
         for cfg_key in flask_config:
             app.config[cfg_key.upper()] = flask_config[cfg_key]
 
-    app.run()
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+
+
+    return app
