483ad9192b485b6bc04442732eb197af47f3808b
Giacomo Salici <giambo2000@gmail.com>
2023-01-08 13:12:03 +0100

    button and wifi

---
+++ bus-stop-button/bus-stop-button.ino
@@ -0,0 +1,58 @@
+#include "wificredential.h"
+/*
+## wificredential.h:
+// Change to your WiFi credentials
+const char* ssid     = "...";
+const char* password = "...";
+*/
+
+#include <WiFi.h>
+
+#define BUTTON_PIN 16 // ESP32 pin GIOP16, which connected to button
+#define LED_PIN 18    // ESP32 pin GIOP18, which connected to led
+
+// variables will change:
+int led_state = LOW;   // the current state of LED
+int button_state;      // the current state of button
+int last_button_state; // the previous state of button
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); // Optional
+    WiFi.begin(ssid, password);
+    Serial.println("\nConnecting");
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+
+    pinMode(BUTTON_PIN, INPUT_PULLUP); // set ESP32 pin to input pull-up mode
+    pinMode(LED_PIN, OUTPUT);          // set ESP32 pin to output mode
+
+    button_state = digitalRead(BUTTON_PIN);
+}
+
+void loop() {
+    last_button_state = button_state;       // save the last state
+    button_state = digitalRead(BUTTON_PIN); // read new state
+
+    if (last_button_state == HIGH && button_state == LOW)
+    {
+        Serial.println("The button is pressed");
+
+        // toggle state of LED
+        led_state = !led_state;
+
+        // control LED arccoding to the toggled state
+        digitalWrite(LED_PIN, led_state);
+    }
+}
